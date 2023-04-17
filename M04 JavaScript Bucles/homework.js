d2d8e73afb8299921a31c9cc03dd909ebb7a8019
sebamansilla <sebastianmansilla222@gmail.com>
2023-04-17 10:00:56 -0300

    tarea

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -130,7 +130,15 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-
+   if(num1 > num2 && num1 > num3 && num1 > 0){
+      return "Numero 1 es mayor y positivo";
+   } else if(num1 < 0 || num2 < 0 || num3 < 0){
+      return "Hay negativos";
+   } else if (num3 > num1 && num3 > num2){
+      return num3 + 1;
+   } else if(num1 === 0 && num2 === 0 && num3 === 0){
+      return "Error";
+   } return false;
 }
 
 function esPrimo(num) {
